c4bff62de3f36cf8f7294aa292d3cdc978b4e2a1
unknown <ma.akbari421@gmail.com>
2022-06-01 12:09:21 +0430

    modified nmf

--- NMF.py
+++ NMF.py
@@ -2,7 +2,6 @@
 Topic Modeling with NMF
 '''
 
-import itertools
 from Dataset.Dataset import Dataset
 import numpy as np
 
@@ -13,7 +12,9 @@ class NMF:
         NMF topic modeling class.\n
         Attributes:\n
         -------------
-
+        tfidf: number_of_docs * number_of_words np.array\n
+        W: number_of_docs * number_of_topics np.array\n
+        H: number_of_topics * number_of_words np.array\n
         '''
         pass
 
@@ -34,5 +35,49 @@ class NMF:
                 tf[i][j] = tf[i][j] * np.round(np.log((N / dataset.words_count[dataset.index_to_word[j]])), 3)
         return tf
     
-    def fit(self, dataset:Dataset, iter:int, stop:float) -> None:
+    def __frobenius_norm(self, v:np.array, w:np.array, h:np.array) -> float:
+        return np.linalg.norm(
+            (v - np.matmul(w, h)), ord='fro'
+        )
+
+    def __update_w(self, v:np.array, w:np.array, h:np.array) -> np.array:
+        numerator = np.matmul(v, np.transpose(h))
+        denomerator = np.matmul(w, np.matmul(h, np.transpose(h)))
+        return np.matmul(w, numerator / denomerator)
+
+    def __update_h(self, v:np.array, w:np.array, h:np.array) -> np.array:
+        numerator = np.matmul(np.transpose(w), v)
+        denomerator = np.matmul(np.transpose(w), np.matmul(h, np.transpose(w)))
+        return np.matmul(h, numerator / denomerator)
+
+    def fit(self, dataset:Dataset, n_topics:int, n_iterations:int=20, stop:float=0.001) -> np.array:
+        '''
+        applies NMF for topic modeling on given dataset
+        Parameters:\n
+        -------------
+        dataset: Dataset obj\n
+        n_topics: int\n
+            number of topics\n
+        n_iterations: int\n
+            number of iterations at most we want NMF go through\n
+        stop: float\n
+            convergence condition\n
+        Return:\n
+        -----------
+        np.array : number_of_docs * number_of_topics np.array
+        '''
         self.tfidf = self.__calculate_tfidf(dataset, self.__calculate_tf(dataset))
+
+        self.W = np.random.uniform(0, 1, (len(dataset.train_corpus), n_topics))
+        self.H = np.random.uniform(0, 1, (n_topics, len(dataset.vocab)))
+
+        for _ in range(n_iterations):
+            loss = self.__frobenius_norm(self.tfidf, self.W, self.H)
+            if loss <= stop:
+                print('model converged...')
+                return self.W
+            self.W = self.__update_w(self.tfidf, self.W, self.H)
+            self.H = self.__update_h(self.tfidf, self.W, self.H)
+        
+        print('end of iterations...')
+        return self.W
