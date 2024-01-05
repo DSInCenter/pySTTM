9771af0dfa85a2996fbb90122ae22649cd076a51
akbari59 <mohammad@deep-medical.ai>
2024-01-05 17:14:18 +0000

    Add setup tools

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'my_python_package',
+    packages = ['my_python_package'],
+    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = '',
+    author_email = '',
+    url = 'github package source url',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
